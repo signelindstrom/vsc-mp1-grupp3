a3d61b48d020b0591336acaba6ffad93c0c4f599
signe <signe.lindstrom@hotmail.com>
2023-01-16 15:12:46 +0100

    weather data url

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ function showPosition(position) {
 
     console.log(`Latitude ${latitude}, Longitude ${longitude}`);
 
+    // get location name
     function getCityName(event){
       const url = `http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&appid=a2de5014979b69e8f9f100296b649487`;
 
@@ -16,6 +17,17 @@ function showPosition(position) {
       .then(response=> response.json)
       .then()
     }
+
+    function getWeather(){
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=a2de5014979b69e8f9f100296b649487`;
+
+      fetch(url)
+      .then(response=> response.json)
+      .then(weatherData)
+    }
+}
+
+function weatherData(){
 }
 
 //if getCurrentPosition failed
