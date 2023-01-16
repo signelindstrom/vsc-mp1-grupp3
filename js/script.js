b2f07318655e5e41c9bb8ab9c1893b41ef09a27f
signe <signe.lindstrom@hotmail.com>
2023-01-16 14:52:32 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,6 +11,14 @@ function showPosition(position) {
     const longitude = position.coords.longitude;
 
     console.log(`Latitude ${latitude}, Longitude ${longitude}`);
+
+    function getCityName(event){
+      const url = `http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&appid=a2de5014979b69e8f9f100296b649487`;
+
+      fetch(url)
+      .then(response=> response.json)
+      .then()
+    }
 }
 
 getLocation();
