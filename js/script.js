301145df1e81de9b44600bf1396a95c0791b0e37
signe <signe.lindstrom@hotmail.com>
2023-01-16 15:46:24 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,36 +1,49 @@
 //get the latitude and longitude of users device
 function getLocation() {
-    navigator.geolocation.getCurrentPosition(showPosition, displayError);
+  navigator.geolocation.getCurrentPosition(showPosition, displayError);
 }
 
 function showPosition(position) {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
 
-    console.log(`Latitude ${latitude}, Longitude ${longitude}`);
+  console.log(`Latitude ${latitude}, Longitude ${longitude}`);
 
-    // get location name
-    function getCityName(event){
-      const url = `http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&appid=a2de5014979b69e8f9f100296b649487`;
+  getCityName(latitude, longitude)
+  getWeather(latitude, longitude);
 
-      fetch(url)
-      .then(response=> response.json())
-      .then()
-    }
-
-    // get weather data
-    function getWeather(){
-      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=a2de5014979b69e8f9f100296b649487`;
-
-      fetch(url)
-      .then(response=> response.json())
-      .then()
-    }
 }
 
+
+// get location name
+function getCityName(lat, long) {
+  const url = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${long}&appid=a2de5014979b69e8f9f100296b649487`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(locationData)
+}
+
+function locationData(locationName){
+  const name = document.querySelector('#current-location');
+  console.log(locationName[0].name)
+  name.innerText = locationName[0].name
+}
+
+
+// get weather data
+function getWeather(lat, long) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=a2de5014979b69e8f9f100296b649487`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then()
+}
+
+
 //if getCurrentPosition failed
-function displayError(){
-    console.log('Geolocation is not supported by this browser');
+function displayError() {
+  console.log('Geolocation is not supported by this browser');
 }
 
 getLocation();
